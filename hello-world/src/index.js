07c8694c225b4457eb13e0a97a4fd7b71c8c8605
shashank.keshava <shashank.keshava@bookmyshow.com>
2017-05-03 16:13:21 +0530

    Composition

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -360,7 +360,54 @@ class TemperatureInput extends React.Component {
 }
 
 
+//Composition VS Inheritence
+
+function FancyBorder(props) {
+    return (
+        <div className={'FancyBorder FancyBorder-' + props.color}>
+      {props.children}
+    </div>
+    );
+}
+
+
+function Dialog(props) {
+    return (
+        <FancyBorder color="blue">
+        <h1 className="Dialog-title">
+        {props.title}
+        </h1>
+        <p className="Dialog-message">
+        {props.message}
+        </p>
+        {props.children}
+        </FancyBorder>
+    );
+}
+
+class SignUpDialog extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { login: '' };
+    }
+    handleChange = (e) => {
+        this.setState({ login: e.target.value });
+    }
+    handleSignUp = () => {
+        alert(`Welcome Aboard ${this.state.login}`);
+    }
+    render() {
+        return (
+            <Dialog title="Mars Exploration Program" message="How should we refer you">
+            <input value={this.state.login} onChange={this.handleChange}/>
+            <button onClick={this.handleSignUp}>Sign Me Up!</button>
+            </Dialog>
+        )
+    }
+
+}
+
 const element = <Welcome name="shashank"/>;
-ReactDOM.render(<Calculator/>,
+ReactDOM.render(<SignUpDialog/>,
     document.getElementById('root')
 );
