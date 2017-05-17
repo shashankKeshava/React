5e6246bc3db842a74a529f1f1183b33a59fd514a
shashank.keshava <shashank.keshava@bookmyshow.com>
2017-05-17 17:58:34 +0530

    Changed product components

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -443,24 +443,49 @@ class SearchBar extends React.Component {
 
 class ProductTable extends React.Component {
     render() {
-        return <ProductCategoryRow category={this.props.products}/>
+        let rows = [];
+        let lastCategory = null;
+        this.props.products.forEach((product) => {
+            if (product.category != lastCategory) {
+                rows.push(<ProductCategoryRow category={product.category} key={product.category}/>);
+                rows.push(<ProductRow product={product} key={product.name}/>);
+                lastCategory = product.category;
+
+            }
+
+        })
+
+        return (<table>
+            <thead>
+            <tr>
+            <th>Name</th>
+            <th>Price</th>
+            </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+            </table>)
     }
 }
 
 class ProductCategoryRow extends React.Component {
     render() {
-        return <ProductRow product={this.props.category}/>
+        return <tr><th colspan="2">{this.props.category}</th></tr>
     }
 }
 
 class ProductRow extends React.Component {
     render() {
+        var name = this.props.product.stocked ?
+            this.props.product.name :
+            <span style={{color: 'red'}}>
+        {this.props.product.name}
+      </span>;
         return (
-            <div>
-            
-            Hello
-            </div>
-        )
+            <tr>
+        <td>{name}</td>
+        <td>{this.props.product.price}</td>
+      </tr>
+        );
     }
 }
 
